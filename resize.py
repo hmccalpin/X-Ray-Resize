4181c685eb526839eac59b8f908c16238798c9fa
hmccalpin <44852403+hmccalpin@users.noreply.github.com>
2018-12-12 12:06:57 -0800

    Update resize.py

--- resize.py
+++ resize.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 
 #storing list of image files in dirs variable
-path = "./images/"
+path = "hmccalpin/src/data/images/"
 dirs = os.listdir(path)
 
 #creates path for resized image files
