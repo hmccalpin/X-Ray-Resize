cd5c59f5c6e8fda3af7267d473cf1a23e7f0474a
hmccalpin <44852403+hmccalpin@users.noreply.github.com>
2018-12-19 15:14:51 -0800

    Update resize.py

--- resize.py
+++ resize.py
@@ -3,7 +3,7 @@ from PIL import Image
 import sys
 import logging
 
-im = sys.argv[2]
+im = sys.argv[1]
 imResize = im.resize((200,200), Image.ANTIALIAS)
 
 logging.info('resized image: {}'.format(im))
