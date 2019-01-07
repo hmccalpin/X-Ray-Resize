734576b0d1da87588a8efebcf81252693879e5cb
hmccalpin <44852403+hmccalpin@users.noreply.github.com>
2019-01-06 18:49:21 -0800

    Update resize.py

--- resize.py
+++ resize.py
@@ -10,7 +10,7 @@ image = Image.open(image_ID)
 logging.info('resized image: {}'.format(image))
 
 new_image = image.resize((200,200), Image.ANTIALIAS)
-new_image.save('/out/{}'.format(image_ID))
+new_image.save('/out{}'.format(image_ID))
 
 print(image.size) # Output: (1024, 1024)
 print(new_image.size) # Output: (200, 200)
