d257dd5eb1d7aaf4a1da7220cb6287bdb76e8acc
hmccalpin <44852403+hmccalpin@users.noreply.github.com>
2019-01-06 18:08:04 -0800

    Update resize.py

--- resize.py
+++ resize.py
@@ -3,8 +3,7 @@ from PIL import Image
 import sys
 import logging
 
-#image_ID = os.environ['IMAGE_ID']
-image_ID = "/resize-app/images/{}".format(IMAGE_ID)
+image_ID = os.environ['IMAGE_ID']
 
 im = Image.open(image_ID)
 imResize = im.resize((200,200), Image.ANTIALIAS)
